453f0000ab569029905029447ba3f06677e40a63
Anna <ds_anna@bk.ru>
2022-05-23 17:40:34 +0300

    Определить, присутсвует ли в заданном массиве, некоторое число

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Определить, присутсвует ли в заданном массиве, некоторое число
+int[] arr = { 1, 56, 78, 9, 44, -9 };
+for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]} ");
+Console.WriteLine();
+Console.WriteLine("Введите число для поиска в массиве: ");
+int num = int.Parse(Console.ReadLine());
+int check = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] == num)
+    {
+        Console.WriteLine($"Заданное число {num} найдено в массиве ");
+        check = 1;
+        break;
+    }
+}
+if (check == 0) Console.WriteLine($"Заданное число {num} не найдено в массиве ");
+
+// Второй вариант
+//int[] arr = { 1, 56, 78, 9, 44, -9 };
+//int number = 56;
+//string result = "";
+//foreach (int var in arr)
+//{
+//    if (var == number)
+//    {
+//        result = "Есть!";
+//        break;
+//    }
+//    else
+//    {
+//        result = "Нет!";
+//    }
+//}
+//Console.WriteLine(result);
